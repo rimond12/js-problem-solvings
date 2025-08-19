1b007e373ac4da1e298d6489c46b77c345391cca
Your Namerimond12 <rimondey010@gmail.com>
2025-08-19 23:07:34 +0600

    added removeDuplicate solving

--- index.js
+++ index.js
@@ -43,3 +43,17 @@ function findMaxNum(arr) {
 }
 
 // console.log(findMaxNum([5, 1, 9, 3]));
+
+// problem-5: Remove duplicates from an Array
+function removeDuplicates(arr){
+    let unique = [];
+    for (let num of arr){
+        if(!unique.includes(num)){
+            unique.push(num);
+        }
+    }
+    return unique;
+}
+
+// console.log(removeDuplicates([1,2,2,3,4,4]));
+
